cbeeb9b0ac50753dcf3a53a0e0f1f55855b343af
Mostafa Zaghlool <148554975+Mostafazaghlool2011@users.noreply.github.com>
2023-10-21 19:17:33 +0300

    Update DIO_interface.h

--- MCAL/DIO/DIO_interface.h
+++ MCAL/DIO/DIO_interface.h
@@ -6,16 +6,16 @@
 */
 
 
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
+#ifndef MCAL_DIO_INTERFACE_H
+#define MCAL_DIO_INTERFACE_H
 
 
 void DIO_voidSetPortDirection		(u8 Copy_u8PortID ,u8 Copy_u8PortDif);
-void DIO_voidSetPortValue			(u8 Copy_u8PortID ,u8 Copy_u8PortValue);
+void DIO_voidSetPortValue		(u8 Copy_u8PortID ,u8 Copy_u8PortValue);
 void DIO_voidSetPinDirection		(u8 Copy_u8PortID ,u8 Copy_u8PinId,u8 Copy_u8PinDir);
-void DIO_voidSetPinValue			(u8 Copy_u8PortID ,u8 Copy_u8PinId,u8 Copy_u8PinValue);
-void DIO_u8GetPinValue				(u8 Copy_u8PortID ,u8 Copy_u8PinId);
-void DIO_voidTogglePin				(u8 Copy_u8PortID ,u8 Copy_u8PinId);
+void DIO_voidSetPinValue		(u8 Copy_u8PortID ,u8 Copy_u8PinId,u8 Copy_u8PinValue);
+void DIO_u8GetPinValue			(u8 Copy_u8PortID ,u8 Copy_u8PinId);
+void DIO_voidTogglePin			(u8 Copy_u8PortID ,u8 Copy_u8PinId);
 
 
 #endif
